40ddda9697cf04f2345db5b7f72943e2bd2418f0
Benwin <benwingeorge@gmail.com>
2024-12-05 00:52:09 +0530

    Added projects

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -1,37 +1,52 @@
-import Link from "next/link";
-import { Eye, View } from "lucide-react";
-
 type Props = {
-	project: { title: string; description: string; date?: number; views: number};
-	views: number;
+  project: {
+    title: string;
+    description: string;
+    date?: number;
+    views: number;
+    stacks: string[];
+    image?: string;
+    githubUrl?: string;
+  };
 };
 
-export const Article: React.FC<Props> = ({ project, views }) => {
-	return (
-			<article className="p-4 md:p-8">
-				<div className="flex justify-between gap-2 items-center">
-					<span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
-						{project.date ? (
-							<time dateTime={new Date(project.date).toISOString()}>
-								{Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
-									new Date(project.date),
-								)}
-							</time>
-						) : (
-							<span>SOON</span>
-						)}
-					</span>
-					<span className="text-zinc-500 text-xs  flex items-center gap-1">
-						<Eye className="w-4 h-4" />{" "}
-						{Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
-					</span>
-				</div>
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-					{project.title}
-				</h2>
-				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
-					{project.description}
-				</p>
-			</article>
-	);
+export const Article: React.FC<Props> = ({ project }) => {
+  return (
+    <article className="p-4 md:p-8">
+      <div className="flex justify-between gap-2 items-center"></div>
+      <div className="flex justify-between items-center">
+        <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+          {project.title}
+        </h2>
+		{/* Uncomment to show the github logo */}
+        {/* {project.githubUrl && (
+          <a href={project.githubUrl} target="_blank">
+            <img
+              src="./github.png"
+              alt="Project"
+              className="w-[40px] h-[40px]"
+            />
+          </a>
+        )} */}
+      </div>
+      <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
+        {project.description}
+      </p>
+      <div className="mt-4">
+        {project.stacks.map((el) => (
+          <div className="bg-gray-800 p-2 mr-4 mb-4 rounded-lg shadow-md inline-block max-w-full">
+            <p className="text-gray-400 text-sm">{el}</p>
+          </div>
+        ))}
+      </div>
+	  {/* Uncomment to show the Project image */}
+      {/* {project.image && (
+        <img
+          src={project.image}
+          alt="Project"
+          className="w-full h-auto object-cover rounded-lg mt-4"
+        />
+      )} */}
+    </article>
+  );
 };
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,118 +2,123 @@ import React from "react";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 import { Article } from "./article";
-import { Eye } from "lucide-react";
 
 export const revalidate = 60;
 export default async function ProjectsPage() {
-
-  const mockProjects = [
+  const projects = [
     {
       key: 1,
-      title: "Code Quest",
+      title: "Music NFT",
       description:
-        "Embark on a thrilling journey through the coding realm. Join our hero as they conquer challenges and unravel the mysteries of programming.",
+        "Spearheaded the development and enhancement of a Music NFT Marketplace, integrating Web3 wallet-less payments and multi-blockchain NFT support, while maintaining high coding standards and optimized deployment strategies.",
       date: 1678742400000,
       views: 5,
-    },
-    {
-      key: 2,
-      title: "Data Dance",
-      description:
-        "Witness the enchanting dance of data elements. From graceful strings to rhythmic numbers, our JSON dancers twirl and spin, creating a symphony of structured information.",
-      date: 1678742400000,
-      views: 8,
+      image: "/nft-image.jpg",
+      stacks: [
+        "Next.js",
+        "Nest.js",
+        "MongoDB",
+        "OpenZepplin",
+        "Solidity",
+        "Arweave",
+      ],
     },
     {
       key: 3,
-      title: "Pixel Poetry",
+      title: "Web3 hiring platform",
       description:
-        "Explore the artistry of pixels in this poetic journey. Each pixel tells a story, and together they create a masterpiece of digital expression.",
+        "A Polygon based web3 platform revolutionizing hiring: seamlessly connecting companies with deserving candidates",
       date: 1678742400000,
-      views: 3,
-    },
-    {
-      key: 4,
-      title: "Byte Breeze",
-      description:
-        "Feel the refreshing byte breeze as you navigate the digital landscape. Let the bits and bytes guide you through a seamless and breezy coding experience.",
-      date: 1678742400000,
-      views: 7,
-    },
-    {
-      key: 5,
-      title: "Crypto Chronicles",
-      description:
-        "Delve into the fascinating world of cryptocurrencies. Join our virtual explorer as they unveil the tales and adventures within the blockchain.",
-      date: 1678742400000,
-      views: 2,
+      views: 5,
+      image: "/bidverse.jpg",
+      githubUrl: "https://github.com/Praneetha35/BidVerse",
+      stacks: ["React", "Arweave", "OpenZepplin", "Solidity"],
     },
     {
       key: 6,
-      title: "Bug Ballet",
+      title: "Profile Website",
       description:
-        "Experience the elegant ballet of bugs. Watch as they pirouette through the code, creating a dance of debugging and choreographed fixes.",
+        "My portfolio website - to flaunt my projects and skills, because let's face it, my work deserves its own stage!",
       date: 1678742400000,
-      views: 9,
-    },
-    {
-      key: 7,
-      title: "Infinite Loops",
-      description:
-        "Embark on a mesmerizing journey through infinite loops. Explore the beauty and challenges of cyclical patterns in the ever-expanding digital universe.",
-      date: 1678742400000,
-      views: 4,
+      views: 5,
+      image: "/portfolio.png",
+      githubUrl: "https://github.com/Praneetha35/my-digital-showcase",
+      stacks: ["React", "Next.js", "Tailwind CSS"],
     },
     {
       key: 8,
-      title: "Pixel Pioneers",
+      title: "Polymesh NFT",
       description:
-        "Join the pioneers of pixel artistry. Discover the pixelated landscapes created by visionary artists pushing the boundaries of digital creativity.",
+        "Executed the first-ever Polymesh blockchain mainnet NFT drop, integrating Web3 credit card payments for seamless user experience.",
       date: 1678742400000,
-      views: 6,
+      views: 7,
+      image: "/npm.jpeg",
+      githubUrl: "https://github.com/Praneetha35/npm-form-checker",
+      stacks: ["@polymesh-sdk", "Next.js", "Node JS"],
     },
     {
       key: 9,
-      title: "Code Canvas",
+      title: "One-click Auto deployment",
       description:
-        "Paint your code on the digital canvas. Let your creativity flow as you brush strokes of logic and syntax to create a masterpiece of programming.",
+        "One-click Auto-deployment system using AWS CloudFormation, enabling seamless full-stack application setup with email notifications and secure credential management.",
       date: 1678742400000,
-      views: 1,
+      views: 7,
+      image: "/pantry.jpg",
+      githubUrl: "https://github.com/Praneetha35/pantry-pal",
+      stacks: ["Next.js", "AWS CF", "AWS S3", "AWS EC2", "AWS Secrets Manager"],
     },
     {
-      key: 10,
-      title: "Binary Beats",
+      key: 2,
+      title: "Decentralized Exchange",
       description:
-        "Groove to the binary beats of the digital realm. Each 0 and 1 contributes to the rhythm, creating a symphony of electronic harmony.",
+        "First DEX on Algorand, customizing smart contracts and delivering features based on client requirements through iterative deployments",
       date: 1678742400000,
-      views: 10,
+      views: 5,
+      image: "/market-research.jpeg",
+      stacks: [
+        "Next.js",
+        "Express.js",
+        "MongoDB",
+        "TEAL",
+      ],
+    },
+    {
+      key: 4,
+      title: "Distributed document uploader",
+      description:
+        "Built a Web3 application for secure PDF modification and upload to IPFS, with cryptographic signing for on-chain verification to ensure tamper-proof document integrity.",
+      date: 1678742400000,
+      views: 3,
+      image: "/snake-game.jpg",
+      githubUrl: "https://github.com/Praneetha35/pixel-python",
+      stacks: ["React Js", "Nest.js", "Solidity"],
+    },
+    {
+      key: 5,
+      title: "3D video game",
+      description:
+        "3D third-person free-roaming game, creating immersive environments, engaging gameplay mechanics, and seamless sound integration, earning recognition for innovation and quality.",
+      date: 1678742400000,
+      views: 8,
+      image: "/secure-sign.png",
+      stacks: [
+        "Unity",
+        "C++",
+        "3D Char",
+      ],
+    },
+    {
+      key: 7,
+      title: "Kadena NFT",
+      description:
+        "NFT marketplace on the Kadena blockchain, building and deploying the entire application on AWS, with continuous feature modifications based on client requirements",
+      date: 1678742400000,
+      views: 7,
+      image: "/weather.jpg",
+      githubUrl: "https://github.com/Praneetha35/sky-watch",
+      stacks: ["React", "Pact", "Nest.js"],
     },
   ];
-  // const views = (
-  //   await redis.mget<number[]>(
-  //     ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":"))
-  //   )
-  // ).reduce((acc, v, i) => {
-  //   acc[allProjects[i].slug] = v ?? 0;
-  //   return acc;
-  // }, {} as Record<string, number>);
-
-  const featured = mockProjects.find((project) => project.key === 1)!;
-  const top2 = mockProjects.find((project) => project.key === 2)!;
-  const top3 = mockProjects.find((project) => project.key === 3)!;
-  const sorted = mockProjects
-    // .filter((p) => p.published)
-    .filter(
-      (project) =>
-        project.key !== featured.key &&
-        project.key !== top2.key &&
-        project.key !== top3.key
-    )
-    .sort(
-      (a, b) =>
-        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
-    );
 
   return (
     <div className="relative pb-16">
@@ -127,83 +132,31 @@ export default async function ProjectsPage() {
             Some of the projects are from work and some are on my own time.
           </p>
         </div>
-        <div className="w-full h-px bg-zinc-800" />
-
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-          <Card>
-              <article className="relative w-full h-full p-4 md:p-8">
-                <div className="flex items-center justify-between gap-2">
-                  <div className="text-xs text-zinc-100">
-                    {featured.date ? (
-                      <time dateTime={new Date(featured.date).toISOString()}>
-                        {Intl.DateTimeFormat(undefined, {
-                          dateStyle: "medium",
-                        }).format(new Date(featured.date))}
-                      </time>
-                    ) : (
-                      <span>SOON</span>
-                    )}
-                  </div>
-                  <span className="flex items-center gap-1 text-xs text-zinc-500">
-                    <Eye className="w-4 h-4" />{" "}
-                    {Intl.NumberFormat("en-US", { notation: "compact" }).format(
-                      featured.views
-                    )}
-                  </span>
-                </div>
-
-                <h2
-                  id="featured-post"
-                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
-                >
-                  {featured.title}
-                </h2>
-                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-                  {featured.description}
-                </p>
-                <div className="absolute bottom-4 md:bottom-8">
-                  <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
-                    Read more <span aria-hidden="true">&rarr;</span>
-                  </p>
-                </div>
-              </article>
-          </Card>
-
-          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-            {[top2, top3].map((project, k) => (
-              <Card key={k}>
-                <Article project={project} views={project.views} />
-              </Card>
-            ))}
-          </div>
-        </div>
-        <div className="hidden w-full h-px md:block bg-zinc-800" />
-
         <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
           <div className="grid grid-cols-1 gap-4">
-            {sorted
+            {projects
               .filter((_, i) => i % 3 === 0)
               .map((project) => (
                 <Card key={project.key}>
-                  <Article project={project} views={project.views} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
           <div className="grid grid-cols-1 gap-4">
-            {sorted
+            {projects
               .filter((_, i) => i % 3 === 1)
               .map((project) => (
                 <Card key={project.key}>
-                  <Article project={project} views={project.views} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
           <div className="grid grid-cols-1 gap-4">
-            {sorted
+            {projects
               .filter((_, i) => i % 3 === 2)
               .map((project) => (
                 <Card key={project.key}>
-                  <Article project={project} views={project.views} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
